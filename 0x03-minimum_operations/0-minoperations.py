a93d0b91a293db00139f9ff53561dcf3226ef09b
mlaizure <maddi.laizure@gmail.com>
2021-08-11 05:26:03 +0000

    Factoring out only primes

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """module with minOperations function"""
+import math
 
 
 def minOperations(n):
+    """finds min operations to paste n number of Hs into a file using only
+    Copy All and Past operations"""
+
+    primeFactors = []
+
+    while n % 2 == 0:
+        primeFactors.append(2)
+        n = n // 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            primeFactors.append(i)
+            n = n // i
+
+    if n > 2:
+        primeFactors.append(n)
+
+    return sum(primeFactors)
+
     """finds min operations to paste n number of Hs into a file using only
     Copy All and Paste operations.
 
@@ -10,7 +30,7 @@ def minOperations(n):
     factors come before it in the loop.
 
     it works fine for reasonably small numbers and is a win for readability
-    imo. would factor out the prime numbers if performance became an issue."""
+    imo. would factor out the prime numbers if performance became an issue.
 
     numOps = 0
     for i in range(2, n // 2 + 1):
@@ -18,3 +38,4 @@ def minOperations(n):
             numOps += i
             n = n / i
     return numOps
+"""
