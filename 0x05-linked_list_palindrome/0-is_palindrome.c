6386cf877357e044bd5e65052e93b3a684b55f2a
mlaizure <maddi.laizure@gmail.com>
2021-08-25 21:20:17 +0000

    FASTER

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * reverse_first_half - reverses first half of the singly linked list
+ * @head: pointer to pointer to head of list
+ * @len: length of list
+ * @i: incrementer
+ * Return: pointer to last node in list
+ */
+
+listint_t *reverse_first_half(listint_t **head, int len, int i)
+{
+	listint_t *after, *current;
+
+	current = *head;
+	if (i < len / 2 - 1 && (*head)->next)
+	{
+		*head = (*head)->next;
+		after = reverse_first_half(head, len, i + 1);
+		after->next = current;
+		return (current);
+	}
+	return (current);
+}
+
+/**
  * is_palindrome - determines if singly linked list is a palindrome
  * @head: pointer to pointer of first node of listint_t list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -8,8 +31,8 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head, *start = *head, *end = *head;
-	int len = 0, i = 0, j = 0;
+	listint_t *current = *head, *first, *back = *head, *p;
+	int len = 0, i = 0;
 
 	if (!head || !(*head))
 		return (1);
@@ -20,18 +43,28 @@ int is_palindrome(listint_t **head)
 		len++;
 	}
 
-	for (i = 0; i < len / 2; ++i)
+	for (i = 0; i < len / 2 - 1; ++i)
+		back = back->next;
+
+	if (len % 2 == 0)
+		back = back->next;
+	else
 	{
-		j = i + 1;
-		end = *head;
-		while (j < len)
-		{
-			end = end->next;
-			++j;
-		}
-		if (start->n != end->n)
+		back = back->next;
+		back = back->next;
+	}
+
+	p = reverse_first_half(head, len, 0);
+	p->next = NULL;
+
+	first = *head;
+
+	while (back)
+	{
+		if (first->n != back->n)
 			return (0);
-		start = start->next;
+		first = first->next;
+		back = back->next;
 	}
 
 	return (1);
