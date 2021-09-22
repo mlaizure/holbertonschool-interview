f47e37178f0568ee843790bcba24b3315e84464f
mlaizure <maddi.laizure@gmail.com>
2021-09-22 17:48:50 +0000

    Fixing off by 1 issue

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -22,11 +22,11 @@ def readValidUTF8Char(data):
     if hasBitsPrefix(data[0], '0'):
         return data[1:]
     elif hasBitsPrefix(data[0], '110'):
-        return readValidUTF8SuccessorBytes(data[1:], 2)
+        return readValidUTF8SuccessorBytes(data[1:], 1)
     elif hasBitsPrefix(data[0], '1110'):
-        return readValidUTF8SuccessorBytes(data[1:], 3)
+        return readValidUTF8SuccessorBytes(data[1:], 2)
     elif hasBitsPrefix(data[0], '11110'):
-        return readValidUTF8SuccessorBytes(data[1:], 4)
+        return readValidUTF8SuccessorBytes(data[1:], 3)
     else:
         return False
 
