fdc650e2ba7d7c28cf4e982a2cd3cdd7d4611434
mlaizure <maddi.laizure@gmail.com>
2021-10-21 04:26:52 +0000

    Draft

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -22,5 +22,73 @@ def get_n():
     return n
 
 
+def row_has_queen(board, row):
+    """check row for other queens"""
+    for has_queen in board[row]:
+        if has_queen:
+            return True
+    return False
+
+
+def col_has_queen(board, col):
+    """check col for other queens"""
+    n = len(board[0])
+    for row in range(n):
+        has_queen = board[row][col]
+        if has_queen:
+            return True
+    return False
+
+
+def diag_has_queen(board, row, col):
+    """check diagonals for queens"""
+    for i, j in zip(range(row, -1, -1),
+                    range(col, -1, -1)):
+        if board[i][j]:
+            return True
+
+    for i, j in zip(range(row, len(board[0]), 1),
+                    range(col, -1, -1)):
+        if board[i][j]:
+            return True
+
+    return False
+
+
+def print_board(board, n):
+    """prints the board"""
+    queens = [[i, j] for i in range(n) for j in range(n) if board[i][j]]
+    print(queens)
+
+
+def n_queens():
+    """make board"""
+    n = get_n()
+    board = [[False for j in range(n)] for i in range(n)]
+    solve_nqueens(0, 0, board)
+
+
+def solve_nqueens(idx, queens_found, board):
+    """recursive backtracking"""
+    n = len(board[0])
+    ijs = [(j, i) for j in range(n) for i in range(n)]
+    if queens_found == n:
+        print_board(board, n)
+        return True
+    elif idx >= len(ijs):
+        return False
+    i, j = ijs[idx]
+    if not row_has_queen(board, i) and not col_has_queen(board, j) \
+       and not diag_has_queen(board, i, j):
+        queens_found += 1
+    b_true = [row[:] for row in board]
+    b_false = [row[:] for row in board]
+    b_true[i][j] = True
+    b_false[i][j] = False
+    if solve_nqueens(idx + 1, queens_found, b_true) or \
+       solve_nqueens(idx + 1, queens_found, b_false):
+        return True
+
+
 if __name__ == "__main__":
-    get_n()
+    n_queens()
