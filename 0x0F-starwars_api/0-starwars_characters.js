86e8db38a07d233f2a90d4752c0bd0817a8ea64d
mlaizure <maddi.laizure@gmail.com>
2021-11-08 18:56:20 +0000

    First try

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+const filmUrl = `https://swapi-api.hbtn.io/api/films/${args[2]}`;
+
+request(filmUrl, function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  const characters = JSON.parse(body).characters;
+  const orderedChars = {};
+
+  characters.forEach(character => {
+    request(character, function (error, response, body) {
+      if (error) {
+        return console.log(error);
+      }
+      const name = JSON.parse(body).name;
+      orderedChars[character] = name;
+      if (Object.values(orderedChars).length === characters.length) {
+        characters.forEach(character => {
+          console.log(orderedChars[character]);
+        });
+      }
+    });
+  });
+});
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,6 @@
+# 0x0F. Star Wars API
+Write a script that prints all characters of a Star Wars movie
+- The first positional argument passed is the Movie ID - example: `3` = “Return of the Jedi”
+- Display one character name per line in the same order as the “characters” list in the `/films/` endpoint
+- Use the [Star Wars API](https://swapi-api.hbtn.io/)
+- Use the `request` module
