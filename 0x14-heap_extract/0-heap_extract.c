b4ec2ef99717e9d699bdb0cbf535124c55b82c89
mlaizure <maddi.laizure@gmail.com>
2021-12-14 03:44:32 +0000

    base logic

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -1,67 +1,5 @@
 #include "binary_trees.h"
 
-int check_children(heap_t *node)
-{
-	if (node->left && node->right)
-		return node->right->n > node->left->n;
-	else if (node->left)
-		return (0);
-	else if (node->right)
-		return (1);
-	else
-		return (0);
-}
-
-heap_t *heap_rebuild(heap_t **subtree_root)
-{
-	heap_t *subtree = *subtree_root;
-
-	if (!subtree->left && !subtree->right)
-	{
-		free(*subtree_root);
-		*subtree_root = NULL;
-		return (NULL);
-	}
-	if (check_children(subtree) == 0)
-	{
-		subtree->n = subtree->left->n;
-		heap_rebuild(&subtree->left);
-	}
-	else
-	{
-		subtree->n = subtree->right->n;
-		heap_rebuild(&subtree->right);
-	}
-	return subtree;
-}
-
-short int place_child(heap_t *t, *n) {
-	short int placed_left = 0, placed_right = 0;
-
-	if (!t->left && !t->right) {
-		return 0
-	}
-	else if (!t->left) {
-		t->left->n = n->n;
-		return 1;
-	}
-	else if (!t->right) {
-		t->right->n = n->n;
-		return 1;
-	}
-	else if (n->n > t->n) {
-		placed_left = place_child(t->left);
-		if (!placed_left)
-			placed_right = place_child(t->right);
-
-		if (placed_left || placed_right)
-			return 1;
-		else
-			return 0;
-	}
-	else
-		return 0;
-}
 
 /**
  * heap_extract - extracts the root node of a max binary heap
@@ -71,15 +9,8 @@ short int place_child(heap_t *t, *n) {
 
 int heap_extract(heap_t **root)
 {
-	int root_val = 0;
-/*	heap_t *orig_root = *root;*/
-
 	if (!root || !(*root))
 		return (0);
 
-	root_val = (*root)->n;
-	heap_rebuild(root);
-/*	free(orig_root);*/
-
-	return (root_val);
+	return ((*root)->n);
 }
